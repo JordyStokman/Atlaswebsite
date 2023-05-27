5730bfcc28b6e1124f2f8dbcf07c8ea9d2ac862e
JordyStokman <3028047@aeres.nl>
2023-05-27 16:24:24 +0200

    More maps

---
+++ MapLibre.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+var map = new maplibregl.Map({
+    container: 'map2',
+    style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
+    center: [5.21835, 52.36645], // starting position [lng, lat]
+    zoom: 5 // starting zoom
+});
