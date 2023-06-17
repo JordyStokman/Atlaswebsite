3da1408f445912abb59bf62f7aac7d168a581327
JordyStokman <3028047@aeres.nl>
2023-06-17 21:35:58 +0200

    Geoserver

--- OpenLayers.js
+++ OpenLayers.js
@@ -1,4 +1,3 @@
-// JavaScript source code
 var raster = new ol.layer.Tile({
     source: new ol.source.OSM()
 });
@@ -9,12 +8,23 @@ var vector = new ol.layer.Vector({
     })
 });
 
+var wmsLayer = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+        url: 'http://localhost:8001/geoserver/Atlaswebsite/wms',
+        params: {
+            'LAYERS': 'Atlaswebsite:PostGIS',
+            'TILED': true
+        },
+        serverType: 'geoserver'
+    })
+});
+
 var map = new ol.Map({
-    layers: [raster, vector],
-    target: document.getElementById('map1'),
+    layers: [raster, vector, wmsLayer],
+    target: 'map1',
     view: new ol.View({
         center: ol.proj.fromLonLat([5.21835, 52.36645]),
-        zoom: 12
+        zoom: 7
     })
 });
 
